9ef2301f3378708e0ec4b67564b1332dbe0cd879
thaleshenr1 <161043237+thaleshenr1@users.noreply.github.com>
2024-07-31 08:56:06 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alura-plus
+# missao-ia
---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultados = document.querySelector(".caixa-resultados");
+const textoresultados = document.querySelector(".texto-resultados");
+
+const perguntas =[
+    {
+        enuciado: "pergunta-1",
+        alternativa:(
+            "alternativa-1",
+            "aternativa-2",
+        )
+    }
+]
